0f0bd58a717ef0b36afe6dbab2c176ce51467071
Rafael Batista <rafaelbatista719@gmail.com>
2021-07-17 23:01:03 -0300

    2021-07-17

--- data/1-edx-test-train.R
+++ data/1-edx-test-train.R
@@ -6,6 +6,7 @@ load("./rda/edx-validation.RData")
 set.seed(1, sample.kind = "Rounding")
 
 test_index <- createDataPartition(edx$rating, times = 1, p = 0.5, list = FALSE)
-edx %>% mutate(rating = as.factor(rating))
-test <- edx[test_index, ]
-train <- edx[-test_index, ]
+temp <- edx %>% mutate(rating = as.factor(rating))
+test <- temp[test_index, ]
+train <- temp[-test_index, ]
+rm(test_index, temp)
