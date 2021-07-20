c443e359e495860cc7be2e185336839c6f11211d
Rafael Batista <rafaelbatista719@gmail.com>
2021-07-20 14:33:18 -0300

    Data wrangling

--- data/1-edx-test-train.R
+++ data/1-edx-test-train.R
@@ -26,8 +26,31 @@ edx <- edx %>% extract(title, c("title", "year"), "^(.*)\\s\\((\\d{4})\\)$") %>%
           isWar = str_detect(genres, "War"),
           isWestern = str_detect(genres, "Western"))
 
-#test_index <- createDataPartition(edx$rating, times = 1, p = 0.5, list = FALSE)
+validation <- validation %>% extract(title, c("title", "year"), "^(.*)\\s\\((\\d{4})\\)$") %>% mutate(year = as.integer(year)) %>%
+  mutate (isAction = str_detect(genres, "Action"),
+          isAdventure = str_detect(genres, "Adventure"),
+          isAnimation = str_detect(genres, "Animation"),
+          isChildren = str_detect(genres, "Children"),
+          isComedy = str_detect(genres, "Comedy"),
+          isCrime = str_detect(genres, "Crime"),
+          isDocumentary = str_detect(genres, "Documentary"),
+          isDrama = str_detect(genres, "Drama"),
+          isFantasy = str_detect(genres, "Fantasy"),
+          isFilmNoir = str_detect(genres, "Film-Noir"),
+          isHorror = str_detect(genres, "Horror"),
+          isIMAX = str_detect(genres, "IMAX"),
+          isMusical = str_detect(genres, "Musical"),
+          isMystery = str_detect(genres, "Mystery"),
+          isRomance = str_detect(genres, "Romance"),
+          isSciFi = str_detect(genres, "Sci-Fi"),
+          isThriller = str_detect(genres, "Thriller"),
+          isWar = str_detect(genres, "War"),
+          isWestern = str_detect(genres, "Western"))
 
-#test <- edx[test_index, ]
-#train <- edx[-test_index, ]
-#rm(test_index)
+test_index <- createDataPartition(edx$rating, times = 1, p = 0.5, list = FALSE)
+
+test <- edx[test_index, ]
+train <- edx[-test_index, ]
+rm(test_index)
+
+save.image(file = "./rda/edx-validation-modified.RData")
