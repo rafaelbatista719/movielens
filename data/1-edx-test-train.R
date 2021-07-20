f511eca516c1b1b55a702efc2772cbe7a8e9b2ed
Rafael Batista <rafaelbatista719@gmail.com>
2021-07-20 13:37:16 -0300

    .

--- data/1-edx-test-train.R
+++ data/1-edx-test-train.R
@@ -5,6 +5,12 @@ load("./rda/edx-validation.RData")
 
 set.seed(1, sample.kind = "Rounding")
 
+
+edx <- edx %>% extract(title, c("title", "year"), "^(.*)\\s\\((\\d{4})\\)$") %>% mutate(year = as.integer(year))
+
+
+
+
 #test_index <- createDataPartition(edx$rating, times = 1, p = 0.5, list = FALSE)
 
 #test <- edx[test_index, ]
