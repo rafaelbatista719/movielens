8530e52e0e1c1a3eac5d7122d6d7bb87d1e4acf8
Rafael Batista <rafaelbatista719@gmail.com>
2021-07-20 15:27:20 -0300

    .

--- data/2-base-analysis.R
+++ data/2-base-analysis.R
@@ -4,7 +4,45 @@ RMSE <- function(true_ratings, predicted_ratings){
   sqrt(mean((true_ratings - predicted_ratings)^2))
 }
 
-#First model: Y_u,i = mu + e_u,i
 
-mu_hat <- mean(train$rating)
-naive_rmse <- RMSE(test$rating, mu_hat)
+#Just the average
+
+mu_hat <- mean(train_set$rating)
+naive_rmse <- RMSE(test_set$rating, mu_hat)
+
+rmse_results <- tibble(method = "Just the average", RMSE = naive_rmse)
+
+
+#First model: Movie Effect Model
+
+movie_avgs <- train_set %>%
+  group_by(movieId) %>%
+  summarize(b_i = mean(rating - mu_hat))
+
+predicted_ratings <- mu_hat + test_set %>%
+  left_join(movie_avgs, by='movieId') %>%
+  pull(b_i)
+
+model_1_rmse <- RMSE(predicted_ratings, test_set$rating)
+rmse_results <- bind_rows(rmse_results,
+                          tibble(method="Movie Effect Model",
+                                     RMSE = model_1_rmse ))
+
+
+#Second model: Movie + User Effects Model
+
+user_avgs <- train_set %>%
+  left_join(movie_avgs, by='movieId') %>%
+  group_by(userId) %>%
+  summarize(b_u = mean(rating - mu_hat - b_i))
+
+predicted_ratings <- test_set %>%
+  left_join(movie_avgs, by='movieId') %>%
+  left_join(user_avgs, by='userId') %>%
+  mutate(pred = mu_hat + b_i + b_u) %>%
+  pull(pred)
+
+model_2_rmse <- RMSE(predicted_ratings, test_set$rating)
+rmse_results <- bind_rows(rmse_results,
+                          tibble(method="Movie + User Effects Model",  
+                                     RMSE = model_2_rmse ))
